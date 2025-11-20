4646b4a3d290f637edb3b08ad9ef314186c30f1f
lucasmargui <lucasmargui@outlook.com>
2025-11-20 14:12:49 -0300

    feat: improve code

---
+++ bridge-external/api1/teste.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>API1 - Teste</h1>";
+echo "<p>Você acessou teste.php sem digitar .php!</p>";
+?>
---
+++ bridge-external/api2/teste.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>API2 - Teste</h1>";
+echo "<p>Você acessou teste.php sem digitar .php!</p>";
+?>
